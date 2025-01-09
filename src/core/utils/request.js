8536a15793c108277a05132e2fdf2a88fa1cd021
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-10 01:33:28 +0200

    React <> Add - request function for api calls

---
+++ src/core/utils/request.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8080/";
+
+export const request = async ({ route, method = "GET", body }) => {
+  try {
+    const response = await axios.request({
+      url: `${route}`,
+      method,
+      data: body,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log("======== Error =========");
+    console.log(error);
+    console.log("======== // =========");
+
+    throw error;
+  }
+};
