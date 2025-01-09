93a8a37ac0cb965288e50c3d83374a3d765371e7
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-10 01:28:53 +0200

    React <> Add - usersSlice

---
+++ src/lib/redux/users/slice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const usersInitialState = {
+  users: [],
+  isLoading: false,
+  error: null,
+};
+const usersSlice = createSlice({
+  name: "users",
+  initialState: usersInitialState,
+  reducers: {},
+});
