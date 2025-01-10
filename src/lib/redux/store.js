07e728e7009016d1e4b367fbe735a2c032005c77
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-10 02:33:53 +0200

    React <> Add - auth slice reducer in store

--- src/lib/redux/store.js
+++ src/lib/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./slices/users/slice.js";
+import authReducer from "./slices/auth/slice.js";
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     users: usersReducer,
   },
 });
