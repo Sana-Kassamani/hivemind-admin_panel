4a31763e74877fae0d6c49c46e5e7ad1667de470
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-10 04:29:23 +0200

    React <> Add - hivesSlice to store

--- src/lib/redux/store.js
+++ src/lib/redux/store.js
@@ -3,13 +3,14 @@ import usersReducer from "./slices/users/slice.js";
 import authReducer from "./slices/auth/slice.js";
 import { createLogger } from "redux-logger";
 import apiariesReducer from "./slices/apiaries/slice.js";
-
+import hivesReducer from "./slices/hives/slice.js";
 const logger = createLogger();
 const store = configureStore({
   reducer: {
     auth: authReducer,
     users: usersReducer,
-    apiaries: apiariesSlice,
+    apiaries: apiariesReducer,
+    hives: hivesReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
