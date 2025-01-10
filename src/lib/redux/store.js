59cb587b2eeff99620c53cd7d30ea81d9ca93058
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-10 03:12:32 +0200

    React <> Add - redex-logger middleware for debugging

--- src/lib/redux/store.js
+++ src/lib/redux/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./slices/users/slice.js";
 import authReducer from "./slices/auth/slice.js";
+import { createLogger } from "redux-logger";
 
+const logger = createLogger();
 const store = configureStore({
   reducer: {
     auth: authReducer,
     users: usersReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
