b61da5bfe7af30446fd34e177ddebf6eea0d5f9b
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 01:55:00 +0200

    React <> Add - createOwnersData in services

--- src/lib/components/ownersTable/service.js
+++ src/lib/components/ownersTable/service.js
@@ -1,10 +1,20 @@
 import { Button } from "@mui/material";
 import { capitalize } from "../../../core/utils/capitalize";
 
-export const ownersTable = [
-  { id: "name", label: "Username", minWidth: 170 },
-  { id: "location", label: "Email", minWidth: 200 },
-  { id: "hivesNb", label: "Apiaries Count", minWidth: 100 },
+export const ownersColumns = [
+  { id: "username", label: "Username", minWidth: 170 },
+  { id: "email", label: "Email", minWidth: 200 },
+  { id: "apiariesNb", label: "Apiaries Count", minWidth: 100 },
   { id: "banBtn", label: "", minWidth: 100 },
   { id: "unbanBtn", label: "", minWidth: 100 },
 ];
+
+export function createOwnersData(
+  username,
+  email,
+  apiariesNb,
+  banBtn,
+  unbanBtn
+) {
+  return { username, email, apiariesNb, banBtn, unbanBtn };
+}
