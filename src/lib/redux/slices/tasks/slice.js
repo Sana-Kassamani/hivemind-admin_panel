0ca26fab5732a51862a0dfe24fbbbd9cefcad28e
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-10 04:39:51 +0200

    React <> Add - tasksSlice

---
+++ src/lib/redux/slices/tasks/slice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchApiaries } from "../../thunks/fetchApiaries";
+
+const tasksInitialState = {
+  tasks: {},
+};
+const tasksSlice = createSlice({
+  name: "tasks",
+  initialState: tasksInitialState,
+  reducers: {
+    clear: (current) => {
+      current.tasks = {};
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchApiaries.fulfilled, (state, action) => {
+      action.payload.forEach((apiary) => {
+        const { hives, tasks, ...rest } = apiary;
+        state.tasks[apiary._id] = tasks;
+      });
+    });
+  },
+});
+
+export default tasksSlice.reducer;
