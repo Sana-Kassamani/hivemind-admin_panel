efc71ff787146bd8a8f07bbeeb6e4935c6a67b0b
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 01:47:39 +0200

    React <> Add - theme provider in app.jsx

--- src/lib/App.jsx
+++ src/lib/App.jsx
@@ -1,17 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../ui/base/fonts.css";
 import "../ui/styles/App.css";
 import Login from "./pages/Login";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "../ui/theme/theme";
+import { CssBaseline } from "@mui/material";
+import Panel from "./pages/Panel";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          {/* <div className=""></div> */}
+          <Routes>
+            <Route path="/" element={<Panel />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
 };
