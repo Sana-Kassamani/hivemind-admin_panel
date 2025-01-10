f8f6deddc4cb3c5b670f0544bd8847728a2de0d3
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-10 02:06:54 +0200

    React <> Add - provider for redux redux in app

--- src/lib/App.jsx
+++ src/lib/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import "../ui/styles/App.css";
 import Login from "./pages/Login";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
