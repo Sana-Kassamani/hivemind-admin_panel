015145b3057c60720c014b331057a5c95d8880e6
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 01:46:24 +0200

    React <> Add - colors in theme

---
+++ src/ui/theme/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+
+export const colors = {
+  primary: "#FAD02C",
+  secondary: "#4F1308",
+  textColor: "#050F24",
+  backgroundColor: "#F0F2F5",
+  cardColor: "#fcfcfc",
+  dividerColor: "#E1E1E1",
+};
