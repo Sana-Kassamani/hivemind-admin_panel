6e432b7963e3156d5f17aa033b59643bce9fd862
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 20:55:24 +0200

    React <> Add - apiaryId prop to hivesTable

--- src/lib/components/hivesTable/index.js
+++ src/lib/components/hivesTable/index.js
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import DataTable from "../table";
-import {
-  apiariesColumns,
-  fillApiariesRows,
-  fillHivesRows,
-  hivesColumns,
-} from "./service";
+import { fillHivesRows, hivesColumns } from "./service";
 import { Typography } from "@mui/material";
 
-const HivesTable = () => {
-  const apiaryId = "677ae340d963de5ec1f4c5ce";
+const HivesTable = ({ apiaryId }) => {
   const { hives, isLoading } = useSelector((state) => state.hives);
   const [rows, setRows] = useState([]);
 
