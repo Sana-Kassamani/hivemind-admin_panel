347856775b9f3ec077e6133d4f3a8200a46bb642
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 18:39:04 +0200

    React <> Create - drawer mui as sidebar

--- src/lib/components/sidebar/index.js
+++ src/lib/components/sidebar/index.js
@@ -1,88 +1,130 @@
-import { useTheme } from "@emotion/react";
-import { Box, Divider, Icon, Typography } from "@mui/material";
-import React, { useState } from "react";
-import { ProSidebar, Menu, MenuItem, Sidebar } from "react-pro-sidebar";
-
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import {
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  AccountCircle,
+  AccountCircleOutlined,
+  HiveOutlined,
+  LogoutOutlined,
+  PeopleAlt,
+  PeopleAltOutlined,
+  Person,
+  PersonOutline,
+} from "@mui/icons-material";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { capitalize } from "../../../core/utils/capitalize";
 import { colors } from "../../../ui/theme/theme";
-import { Link } from "react-router-dom";
-import { AccountCircle, Hive, People, Person } from "@mui/icons-material";
+import "./style.css";
+
+const drawerWidth = "20%";
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    padding: "10px 12px",
+    width: drawerWidth,
+  },
+
+  list: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    height: "100%",
+    width: "100%",
+    color: colors.secondary,
+  },
+}));
 const SideBar = () => {
-  const theme = useTheme();
-  const [isCollapsed, setIsCollapsed] = useState(false);
-  const [selected, setSelected] = useState("");
+  const { loggedAdmin } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const classes = useStyles();
+
+  const menuItems = [
+    {
+      text: "Users",
+      icon: <PeopleAltOutlined color="secondary" />,
+      path: "/panel",
+    },
+    {
+      text: "Apiaries",
+      icon: <HiveOutlined color="secondary" />,
+      path: "/apiaries",
+    },
+  ];
   return (
-    <Sidebar>
-      <Box
-        display="flex"
-        gap="10px"
-        p="20px 10px"
-        alignItems="center"
-        color={colors.secondary}
-      >
-        <Person />
-        <Typography variant="h3">Admin Name</Typography>
-      </Box>
-      <Divider />
-      <Menu
-        menuItemStyles={{
-          root: {
-            "&:hover": {
-              backgroundColor: colors.primary,
-              color: colors.textColor,
-            },
-          },
-        }}
-        // menuItemStyles={{
-        //   root: {
-        //     [`&.hover`]: {
-        //       backgroundColor: `${colors.primary}`,
-        //     },
-        //   },
-        //   button: {
-        //     [`&.hover`]: {
-        //       backgroundColor: `${colors.primary}`,
-        //     },
-        //   },
-        // }}
-      >
-        <Item
-          title="Users"
-          to="/"
-          icon={<People htmlColor="" />}
-          selected={selected}
-          setSelected={setSelected}
-        />
-        <Item
-          title="Apiaries"
-          to="/"
-          icon={<Hive />}
-          selected={selected}
-          setSelected={setSelected}
-        />
-        <Item />
-      </Menu>
-    </Sidebar>
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      anchor="left"
+      classes={{ paper: classes.drawerPaper }}
+    >
+      {/* {list links} */}
+      <List className={classes.list}>
+        <div>
+          <ListItem>
+            <ListItemIcon>
+              <AccountCircle color="secondary" />
+            </ListItemIcon>
+            <ListItemText primary={capitalize(loggedAdmin.username)} />
+          </ListItem>
+          <Divider />
+          <div className="listItemDiv">
+            {menuItems.map((item) => (
+              <ListItem
+                key={item.text}
+                button
+                onClick={() => navigate(item.path)}
+                selected={location.pathname === item.path}
+                sx={{
+                  marginTop: "5px",
+                  borderRadius: "10px",
+                  "&:hover": {
+                    backgroundColor:
+                      location.pathname === item.path
+                        ? "primary.dark"
+                        : "primary.light", // Hover background color
+                  },
+                  backgroundColor:
+                    location.pathname === item.path
+                      ? "primary.main"
+                      : "transparent",
+                }}
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))}
+          </div>
+        </div>
+        <div>
+          <Divider />
+          <ListItem
+            button
+            onClick={() => {
+              // logout
+            }}
+          >
+            <ListItemIcon>
+              <LogoutOutlined color="secondary" />
+            </ListItemIcon>
+            <ListItemText primary="Logout" />
+          </ListItem>
+        </div>
+      </List>
+    </Drawer>
   );
 };
 
 export default SideBar;
-
-const Item = ({ title, to, icon, selected, setSelected }) => {
-  return (
-    <MenuItem
-      active={selected === title}
-      style={{
-        color: colors.secondary,
-        "&:hover": {
-          backgroundColor: colors.primary,
-          color: colors.textColor,
-        },
-      }}
-      onClick={() => setSelected(title)}
-      icon={icon}
-      component={<Link to={to} />}
-    >
-      <Typography variant="h5">{title}</Typography>
-    </MenuItem>
-  );
-};
