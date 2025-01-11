6d337877fbd0103d597f874bbcb9ec393b1104ce
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 02:54:46 +0200

    React <> Add - fill hivess rows

--- src/lib/components/hivesTable/service.js
+++ src/lib/components/hivesTable/service.js
@@ -3,3 +3,21 @@ export const hivesColumns = [
   { id: "framesNb", label: "Frames Count", minWidth: 150 },
   { id: "health", label: "Health", minWidth: 100 },
 ];
+
+export function createHivesData(label, framesNb, health) {
+  return { label, framesNb, health };
+}
+
+export const fillHivesRows = ({ setRows, hives, apiaryId }) => {
+  const newRows = [];
+  hives[apiaryId].forEach((hive) => {
+    newRows.push(
+      createHivesData(
+        hive.label,
+        hive.nbOfframes,
+        hive.diseases.length == 0 ? "Health" : "Diseased"
+      )
+    );
+  });
+  setRows(newRows);
+};
