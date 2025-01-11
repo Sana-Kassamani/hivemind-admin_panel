203b97b59a08824acb3c7c25d0add519384a276f
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 20:57:09 +0200

    React <> Add - tasksColumns in tasksTable services

---
+++ src/lib/components/tasksTable/service.js
@@ -0,0 +1,7 @@
+export const tasksColumns = [
+  { id: "title", label: "Title", minWidth: 170 },
+  { id: "content", label: "Content", minWidth: 150 },
+  { id: "comment", label: "Comment", minWidth: 150 },
+  { id: "status", label: "Status", minWidth: 100 },
+  { id: "date", label: "Date", minWidth: 120 },
+];
