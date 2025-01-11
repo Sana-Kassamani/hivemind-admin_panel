b712579498e01b0ee337b343dab130e4cd13c2bd
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 02:11:55 +0200

    React <> Add - beekeepers column in services

---
+++ src/lib/components/beekeepersTable/service.js
@@ -0,0 +1,8 @@
+export const beekeepersColumns = [
+  { id: "username", label: "Username", minWidth: 170 },
+  { id: "email", label: "Email", minWidth: 200 },
+  { id: "owner", label: "Owner", minWidth: 170 },
+  { id: "apiary", label: "Assigned Apiary", minWidth: 170 },
+  { id: "banBtn", label: "", minWidth: 100 },
+  { id: "unbanBtn", label: "", minWidth: 100 },
+];
