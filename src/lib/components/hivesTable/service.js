3ef36bc52444a3dc7f60681a90ae174f92ccccc1
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 02:49:38 +0200

    React <> Add - hives column in services

---
+++ src/lib/components/hivesTable/service.js
@@ -0,0 +1,5 @@
+export const hivesColumns = [
+  { id: "label", label: "Label", minWidth: 170 },
+  { id: "framesNb", label: "Frames Count", minWidth: 150 },
+  { id: "health", label: "Health", minWidth: 100 },
+];
