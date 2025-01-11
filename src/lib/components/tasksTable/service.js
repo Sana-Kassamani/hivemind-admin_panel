5b6427fa72576325250f639c098e65c09bae73fe
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-11 20:57:34 +0200

    React <> Add - fillTasksRows function in tasksTable services

--- src/lib/components/tasksTable/service.js
+++ src/lib/components/tasksTable/service.js
@@ -5,3 +5,25 @@ export const tasksColumns = [
   { id: "status", label: "Status", minWidth: 100 },
   { id: "date", label: "Date", minWidth: 120 },
 ];
+
+export function createTasksData(title, content, comment, status, date) {
+  return { title, content, comment, status, date };
+}
+
+export const fillTasksRows = ({ setRows, tasks, apiaryId }) => {
+  const newRows = [];
+
+  tasks[apiaryId].forEach((task) => {
+    const date = new Date(task.date).toLocaleDateString("en-GB");
+    newRows.push(
+      createTasksData(
+        task.title,
+        task.content,
+        task.comment ?? "-",
+        task.status,
+        date
+      )
+    );
+  });
+  setRows(newRows);
+};
