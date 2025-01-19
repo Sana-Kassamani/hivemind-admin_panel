45ec01e5835379dfbe6aac9bd429f695db17b1b6
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-19 16:24:39 +0200

    React <> Edit - comments in tasks Table

--- src/lib/components/tasksTable/service.js
+++ src/lib/components/tasksTable/service.js
@@ -1,7 +1,7 @@
 export const tasksColumns = [
   { id: "title", label: "Title", minWidth: 170 },
   { id: "content", label: "Content", minWidth: 150 },
-  { id: "comment", label: "Comment", minWidth: 150 },
+  { id: "comment", label: "CommentCount", minWidth: 150 },
   { id: "status", label: "Status", minWidth: 100 },
   { id: "date", label: "Date", minWidth: 120 },
 ];
@@ -19,7 +19,7 @@ export const fillTasksRows = ({ setRows, tasks, apiaryId }) => {
       createTasksData(
         task.title,
         task.content,
-        task.comment ?? "-",
+        task.comments.length ?? "-",
         task.status,
         date
       )
